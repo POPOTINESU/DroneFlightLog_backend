6c2adf391713b91e283b9aba80123b7c5395aab7
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-06-14 23:30:56 +0900

    セッションコントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
-  def index
-    # テスト用でHello Worldを返す
-    render json: { message: 'Deploy_test Hello World!' }
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { user: user }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
