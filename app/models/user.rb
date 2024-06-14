68f9bc35aeae05c813e5e7503d3ed3e9d586e992
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-06-14 23:20:58 +0900

    テスト作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,20 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  # presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true
+  validates :password, presence: true
+  # uniqueness: true
+  validates :email, uniqueness: true
+
+  # min_length:
+  validates :password, length: { minimum: 6 }
+
+  def full_name()
+    "#{last_name} #{first_name}"
+  end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,13 +9,20 @@ RSpec.describe User, type: :model do
   it '名がなければ無効な状態であること' do
     user = FactoryBot.build(:user, first_name: nil)
     user.valid?
-    expect(user.errors[:first_name]).to include('ユーザー名を入力してください')
+    expect(user.errors[:first_name]).to include('名を入力してください')
   end
+  it '性がなければ無効な状態であること' do
+    user = FactoryBot.build(:user, last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include('姓を入力してください')
+  end
+
   it 'メールアドレスがなければ無効な状態であること' do
     user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include('メールアドレスを入力してください')
   end
+
   it '重複したメールアドレスなら無効な状態であること' do
     FactoryBot.create(:user)
     user = User.new(
@@ -25,8 +32,9 @@ RSpec.describe User, type: :model do
       password: 'password'
     )
     user.valid?
-    expect(user.errors[:email]).to include('メールアドレスは、すでに存在します')
+    expect(user.errors[:email]).to include('このメールアドレスは既に登録されています')
   end
+
   it 'ユーザーのフルネームを文字列として返すこと' do
     user = User.new(
       first_name: 'User',
@@ -34,6 +42,6 @@ RSpec.describe User, type: :model do
       email: 'example@mail.com',
       password: 'password'
     )
-    expect(user.name).to eq 'Test User'
+    expect(user.full_name).to eq 'Test User'
   end
 end
