046db142671c09fc8a2664fd69b28de0e48ac169
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-07-13 17:44:44 +0900

    migrateエラー解消

---
+++ db/migrate/20240712134716_create_probrems_fields.rb
@@ -0,0 +1,13 @@
+class CreateProbremsFields < ActiveRecord::Migration[7.1]
+  def change
+    create_table :probrems_fields do |t|
+      t.references :user, null: false, foreign_key: true, type: :uuid
+      t.references :flight_log, null: false, foreign_key: true, type: :uuid
+
+      t.string :problem_description
+      t.date :date_of_resolution_datetime
+      t.string :corrective_action
+      t.timestamps
+    end
+  end
+end
